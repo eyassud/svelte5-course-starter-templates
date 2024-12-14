5f3aa66c93766a52040622a6ec4a31bf2fa33110
daniel eyassu <danieleyassu@hotmail.com>
2024-12-14 17:35:34 -0500

    Section 111 completed.

--- book-nest/src/routes/private/books/[bookId]/+page.svelte
+++ book-nest/src/routes/private/books/[bookId]/+page.svelte
@@ -1,3 +1,77 @@
+<script lang="ts">
+  import { Button } from "$components";
+  import StarRating from "$components/StarRating.svelte";
+  import type { Book } from "$lib/state/user-state.svelte";
+  import Icon from "@iconify/svelte";
+
+  interface BookPageProps {
+    data: { book: Book };
+  }
+
+  let { data }: BookPageProps = $props();
+  let book = $derived(data.book);
+
+  function goBack() {
+    history.back();
+  }
+</script>
+
+{#snippet bookInfo()}
+  <h2 class="book-title mt-m">{book.title}</h2>
+  <p class="book-author">{book.author}</p>
+  <h4 class="mt-m mb-xs semi-bold">Your rating</h4>
+  <StarRating value={book.rating} />
+  <p class="small-font">
+    Click to {book.rating ? "change" : "give"} your rating
+  </p>
+  {#if book.description}
+    <h4 class="mt-m mb-xs semi-bold">Description</h4>
+    <p class="mb-m">{book.description}</p>
+  {:else}
+    <h4 class="mt-m mb-xs semi-bold">No description yet.</h4>
+    <button
+      class="block mb-m"
+      onclick={() => console.log("toggle on the edit mode")}
+      type="button"><p>Click to add one.</p></button
+    >
+  {/if}
+  {#if !book.finished_reading_on}
+    <Button
+      isSecondary={true}
+      onclick={() => console.log("Updating reading status")}
+    >
+      {book.started_reading_on
+        ? "I finished reading this book"
+        : "I started reading this book"}
+    </Button>
+  {/if}
+  {#if book.genre}
+    <h4 class="mt-m mb-xs semi-bold">Genre</h4>
+    <p>{book.genre}</p>
+  {/if}
+{/snippet}
+
+<div class="book-page">
+  <button class="back-button" onclick={goBack}>
+    <Icon icon="ep:back" width={"40"} />
+  </button>
+  <div class="book-container">
+    <div class="book-info">
+      {@render bookInfo()}
+    </div>
+    <div class="book-cover">
+      {#if book.cover_image}
+        <img src={book.cover_image} alt="Book cover" />
+      {:else}
+        <button class="add-cover">
+          <Icon icon="bi:camera-fill" width={"40"} />
+          <p>Add book cover</p>
+        </button>
+      {/if}
+    </div>
+  </div>
+</div>
+
 <style>
   .book-container {
     display: flex;
