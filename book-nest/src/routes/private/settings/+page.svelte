44819dea446f294ca2cbd4c51259d2d8212f6632
daniel eyassu <danieleyassu@hotmail.com>
2024-12-26 18:05:38 -0500

    Section 124 completed.

--- book-nest/src/routes/private/settings/+page.svelte
+++ book-nest/src/routes/private/settings/+page.svelte
@@ -1,3 +1,72 @@
+<script lang="ts">
+  import Button from "$components/Button.svelte";
+  import { getUserState } from "$lib/state/user-state.svelte";
+  import { onMount } from "svelte";
+
+  let userContext = getUserState();
+  let userName = $state(userContext.userName || "");
+  let isEditMode = $state(false);
+  let email = $state(userContext.user?.email || "");
+
+  let averageRating = $derived.by(() => {
+    let totalRating = userContext.allBooks
+      .filter((book) => book.rating)
+      .reduce((acc, book) => {
+        return acc + book.rating!;
+      }, 0);
+    return Math.round(100 * (totalRating / userContext.allBooks.length)) / 100;
+  });
+
+  $effect(() => {
+    if (userContext.userName) {
+      userName = userContext.userName;
+    }
+  });
+</script>
+
+<div class="settings-page">
+  <div class="settings-container">
+    <h2>Settings</h2>
+
+    <h5 class="mt-m mb-xs semi-bold">Username</h5>
+    {#if isEditMode}
+      <input type="text" bind:value={userName} />
+    {:else}
+      <h3>{userName}</h3>
+    {/if}
+
+    <h5 class="mt-m mb-xs semi-bold">Email Address</h5>
+    {#if isEditMode}
+      <input type="text" bind:value={email} />
+    {:else}
+      <h3>{email}</h3>
+    {/if}
+
+    <div class="buttons-container mt-l">
+      <Button
+        isSecondary={true}
+        onclick={() => {
+          isEditMode = !isEditMode;
+        }}>{isEditMode ? "Save Changes" : "Edit"}</Button
+      >
+      <Button isDanger={true} onclick={() => console.log("delete")}
+        >Delete Account</Button
+      >
+    </div>
+  </div>
+  <div class="stats-container">
+    <h5 class="semi-bold">Books in library</h5>
+    <h3>{userContext.allBooks.length}</h3>
+    <h5 class="semi-bold mt-m">Finished Books</h5>
+    <h3>
+      {userContext.allBooks.filter((book) => Boolean(book.finished_reading_on))
+        .length}
+    </h3>
+    <h5 class="semi-bold mt-m">Average rating given</h5>
+    <h3>{averageRating}</h3>
+  </div>
+</div>
+
 <style>
   .settings-page {
     display: flex;
